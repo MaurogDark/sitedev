59a57ed39be691d7ff4b83170dedbfafdc576719
MaurogDark <ratgod@gmail.com>
2025-03-19 21:43:25 +0200

    Fixing italic

--- src/textnode.py
+++ src/textnode.py
@@ -120,6 +120,7 @@ def text_to_textnodes(text):
 	nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
 	nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
 	nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+	nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
 	return nodes
 
 def text_to_html_nodes(text):
